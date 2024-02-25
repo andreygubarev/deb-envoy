ffe5d8b33c7f12a7063aec210c4df8ae79eacbcb
Andrey Gubarev <andrey@andreygubarev.com>
2024-02-25 19:32:58 +0400

    init default package struct

--- run.sh
+++ run.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-ENVOY_VERSION=1.29.1
-ENVOY_ARCH=${ENVOY_ARCH:-linux-x86_64}
-
+export ENVOY_VERSION=1.29.1
+export ENVOY_ARCH=${ENVOY_ARCH:-linux-x86_64}
 
 # create the directory structure
-PKG_NAME=envoy
-PKG_VERSION=${ENVOY_VERSION}
-PKG_RELEASE=0
+export PKG_NAME=envoy
+export PKG_VERSION=${ENVOY_VERSION}
+export PKG_RELEASE=0
 case ${ENVOY_ARCH} in
   linux-x86_64)
-    PKG_ARCH=amd64
+    export PKG_ARCH=amd64
     ;;
   linux-aarch_64)
-    PKG_ARCH=arm64
+    export PKG_ARCH=arm64
     ;;
   *)
     echo "Unsupported architecture: ${ENVOY_ARCH}"
@@ -22,38 +21,15 @@ case ${ENVOY_ARCH} in
     ;;
 esac
 
-PKG_DIR=${PKG_NAME}_${PKG_VERSION}-${PKG_RELEASE}_${PKG_ARCH}
-mkdir -p ${PKG_DIR}/DEBIAN
+export PKG_DIR=${PKG_NAME}_${PKG_VERSION}-${PKG_RELEASE}_${PKG_ARCH}
+cp -R envoy ${PKG_DIR}
 
 # create the control file
-cat > ${PKG_DIR}/DEBIAN/control <<EOF
-Package: ${PKG_NAME}
-Version: ${PKG_VERSION}-${PKG_RELEASE}
-Section: base
-Priority: optional
-Architecture: ${PKG_ARCH}
-Maintainer: Andrey Gubarev <andrey@andreygubarev.com>
-Description: Envoy proxy
-EOF
+envsubst < ${PKG_DIR}/DEBIAN/control > ${PKG_DIR}/DEBIAN/control
 
-# create the directory structure
+# create the binary
 mkdir -p ${PKG_DIR}/usr/bin
-
-# download the envoy binary
 curl -L -o ${PKG_DIR}/usr/bin/envoy https://github.com/envoyproxy/envoy/releases/download/v${ENVOY_VERSION}/envoy-${ENVOY_VERSION}-${ENVOY_ARCH}
-
-# copyright
-mkdir -p ${PKG_DIR}/usr/share/doc/${PKG_NAME}
-cat > ${PKG_DIR}/usr/share/doc/${PKG_NAME}/copyright <<EOF
-Format: http://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
-Upstream-Name: Envoy
-Source: <https://github.com/envoyproxy/envoy>
-
-Files: *
-Copyright: Copyright 2016-2022 Envoy Project Authors
-License: Apache
-/usr/share/common-licenses/Apache-2.0
-EOF
 
 # create the package
 dpkg-deb --build ${PKG_DIR}
